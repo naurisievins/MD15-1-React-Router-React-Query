b8b996fcddf3d85c4b566faac494bb8fedac2e07
naurisievins <nauris.ievins@gmail.com>
2023-02-06 02:58:53 +0200

    Load more added + fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './App.css'
 import Home from './pages/Home'
 import Characters from './pages/Characters'
@@ -7,23 +8,27 @@ import Card_info from './pages/Card_info'
 
 function App() {
 
+  const queryClient = new QueryClient()
+
   return (
     <BrowserRouter>
-      <header>
-        <nav>
-          <NavLink to='/'>Home</NavLink>
-          <NavLink to='characters'>Characters</NavLink>
-          <NavLink to='about'>About</NavLink>
-        </nav>
-      </header>
-      <main>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='characters' element={<Characters />} />
-        <Route path='about' element={<About />} />
-        <Route path="/characters/:id" element={<Card_info />} />
-      </Routes>
-      </main>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <nav>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='characters'>Characters</NavLink>
+            <NavLink to='about'>About</NavLink>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='characters' element={<Characters />} />
+            <Route path='about' element={<About />} />
+            <Route path="/characters/:id" element={<Card_info />} />
+          </Routes>
+        </main>
+      </QueryClientProvider>
     </BrowserRouter>
   )
 }
--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -3,28 +3,51 @@ import styles from './Characters.module.scss'
 import { useState, useEffect } from "react";
 import Card from "../components/Card";
 import type { CardType } from "../types/CardType";
+import { useQuery} from '@tanstack/react-query'
 
 export default function Characters() {
 
-  const [loading, setLoading] = useState(true);
-  const [cardsData, setCardsData] = useState<CardType[]>();
-  const url = 'https://rickandmortyapi.com/api/character/'
+  const [page, setPage] = useState(1);
+  const [lastPage, setLastPage] = useState(1);
+  const [cardData, setCardData] = useState<CardType[]>([]);
+  const url = `https://rickandmortyapi.com/api/character/?page=${page}`
+
+  const getData = () => {
+    const res = axios.get(url)
+    .then(({ data }) => data.results)
+    return res;
+  };
 
   useEffect(() => {
     axios.get(url).then(({ data }) => {
-      setCardsData(data.results)
-      setLoading(false)
-    });
+      setLastPage(data.info.pages)
+    })
   }, []);
 
-  if (loading) {
+  const { data, isLoading, isFetching, isSuccess, isError} = useQuery(["cards", page], getData);
+
+  useEffect(() => {
+    if (data) {
+      setCardData((prevData) => [...prevData, ...data]);
+    }
+  }, [data]);
+
+  if (isLoading) {
     return <h1 className={styles.loading}>Loading...</h1>
+  }
+  if (isError) {
+    return <h1 className={styles.loading}>Something went wrong...</h1>
+  }
+
+  const handleLoadMore = () => {
+    console.log('fetch more called')
+    setPage(prevPage => prevPage + 1);
   }
 
   return (
     <div className={styles.container}>
       <div className={styles.row}>
-        {cardsData && cardsData.map(card => (
+        {isSuccess && cardData.map((card: CardType) => (
           <Card key = {card.id}
                 cId = {card.id}
                 cImage = {card.image}
@@ -35,6 +58,12 @@ export default function Characters() {
           />
         ))}
       </div>
+      <button className={styles.btn}
+              onClick={handleLoadMore}
+              disabled={isLoading || isFetching || page >= lastPage}
+      >
+        {page >= lastPage ? 'All loaded' : isLoading ? 'Loading...' : 'Load More'}
+      </button>
     </div>
   )
 
